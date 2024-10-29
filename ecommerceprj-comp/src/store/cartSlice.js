da546161553d3f5f0fbf686602d441a3d5ee72b9
maryamamirali <maryamamir564@gmail.com>
2024-10-29 17:47:49 +0500

    e-commerce

--- ecommerceprj-comp/src/store/cartSlice.js
+++ ecommerceprj-comp/src/store/cartSlice.js
@@ -15,42 +15,13 @@ addtocart: (state , data) => {
         console.log('Added to cart:', data);
 },   
 
-
-decreaseCart(state, action) {
-    const itemIndex = state.cartItems.findIndex(
-      (item) => item.id === action.payload.id
-    );
-},
-
-
 deltocart: (state , data) => {
     state.cart.pop(data.payload)
     console.log('Removed from cart:', data);
 },
 
 
-getTotals(state, action) {
-    let { total, quantity } = state.cartItems.reduce(
-      (cartTotal, cartItem) => {
-        const { price, cartQuantity } = cartItem;
-        const itemTotal = price * cartQuantity;
-
-        cartTotal.total += itemTotal;
-        cartTotal.quantity += cartQuantity;
-
-        return cartTotal;
-      },
-      {
-        total: 0,
-        quantity: 0,
-      }
-    );
-    total = parseFloat(total.toFixed(2));
-    state.cartTotalQuantity = quantity;
-    state.cartTotalAmount = total;
-  },
-},
-})
+}})
 
 export const { addtocart, deltocart, decreaseCart, getTotals } = cartSlice.actions
 export default cartSlice.reducer
